90eb3acd4c87b083414d2822be53eb9acb53a2a6
Nikhil Kumar Gupta <niks.a3198@gmail.com>
2021-05-17 21:00:20 +0530

    finished

--- pages/createNotes/index.js
+++ pages/createNotes/index.js
@@ -1,5 +1,31 @@
-const createNoteInput = document.querySelector(".create-note-input");
+const createNoteButton = document.querySelector(".create-note-button");
 
-createNoteInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
+const apiUrl = "https://aasf-final-project-backend.herokuapp.com";
+
+const token = localStorage.getItem("jwt");
+
+createNoteButton.addEventListener("click", () => {
+  const content = document.querySelector(".create-note-input").value;
+  const heading = document.querySelector(".create-note-heading").value;
+
+  if (token) {
+    fetch(`${apiUrl}/note/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({ content, heading }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        alert("Error Creating Note!! Re-try....");
+        console.log(err);
+      });
+  }
 });
--- pages/updateNotes/index.js
+++ pages/updateNotes/index.js
@@ -1,10 +1,36 @@
-const createNoteInput = document.querySelector(".create-note-input");
-
-createNoteInput.addEventListener("input", (e) => {
-  console.log(e.target.value);
-});
-
 const urlParams = new URLSearchParams(window.location.search);
 const noteId = urlParams.get("noteId");
 
 console.log(noteId);
+
+const updateNoteButton = document.querySelector(".create-note-button");
+
+const apiUrl = "https://aasf-final-project-backend.herokuapp.com";
+
+const token = localStorage.getItem("jwt");
+
+updateNoteButton.addEventListener("click", () => {
+  const content = document.querySelector(".create-note-input").value;
+  const heading = document.querySelector(".create-note-heading").value;
+
+  if (token) {
+    fetch(`${apiUrl}/note/update/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({ content, heading }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        alert("Error Creating Note!! Re-try....");
+        console.log(err);
+      });
+  }
+});
